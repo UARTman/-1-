5a0a5fee98f7691aad914fae9d4f9ef580cd34dc
Anton Gusev <agus179e@gmail.com>
2018-10-02 17:46:11 +0300

    Попытка менять

--- ver.0/DrawGR.py
+++ ver.0/DrawGR.py
@@ -4,6 +4,7 @@ Created on Thu Sep 27 15:56:34 2018
 
 @author: UARTman
 """
+import math
 import PIL
 import aggdraw
 _w,_h,_r=500,500,100
@@ -13,18 +14,17 @@ def dncntr(n,k):
     return (_w/2+lx,_h/2+ly)
 
 
-def circle(img,c,r,pen,brush):
-    img.ellipse((c[0]+r,c[1]+r,c[0]-r,c[1]-r),pen,brush)
+def circle(img,c,r,pen):
+    img.ellipse((c[0]+r,c[1]+r,c[0]-r,c[1]-r),pen)
 
 
-def mcrk(img,n,r,pen,brush):
+def mcrk(img,n,r,pen):
     for i in range(0,n):
-        circle(img,dncntr(n,i),r,pen,brush)
-'''
+        circle(img,dncntr(n,i),r,pen)
+
 a=PIL.Image.new('RGBA',(500,500),(0,0,0,0))
 d=aggdraw.Draw(a)
 p=aggdraw.Pen('Black',0.5)
-b = aggdraw.Brush('White')
-mcrk(d,5,10,p,b)
+#b = aggdraw.Brush('White')
+mcrk(d,5,10,p)
 d.flush()
-'''
